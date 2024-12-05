8ced1e1004a3ab74c4d8d14b5a63e87ee6ac19b4
NANDIT SHARMA <137511995+nandit9893@users.noreply.github.com>
2024-12-06 00:38:37 +0530

    Update Navbar.jsx

--- sharma-bookings/src/Components/Navbar.jsx
+++ sharma-bookings/src/Components/Navbar.jsx
@@ -58,8 +58,8 @@ const Navbar = () => {
           </button>
         </div>
         <div className="flex sm:gap-5 gap-0">
-          <Link to="/"><p className="text-slate-700 hover:underline hover:text-gray-500 font-semibold text-xl cursor-pointer">HOME</p></Link>
-          <Link to="/about"><p className="hidden sm:inline text-slate-700 hover:underline hover:text-gray-500 transition-all 0.5s font-semibold text-xl cursor-pointer">ABOUT</p></Link>
+          <Link to="/"><p className="hidden sm:inline text-slate-700 hover:underline hover:text-gray-500 font-semibold text-xl cursor-pointer">HOME</p></Link>
+          <Link to="/about"><p className="text-slate-700 hover:underline hover:text-gray-500 transition-all 0.5s font-semibold sm:text-xl text-xs cursor-pointer">ABOUT</p></Link>
           {
             currentUser ?
             ( currentUser.role === "hotel-owner" ? 
@@ -69,7 +69,7 @@ const Navbar = () => {
             )
             :
             (
-              <Link to="/login-user"><p className="text-slate-700 hover:underline hover:text-gray-500 transition-all 0.5s font-semibold text-xl cursor-pointer">LOGIN</p></Link>
+              <Link to="/login-user"><p className="text-slate-700 hover:underline hover:text-gray-500 transition-all 0.5s font-semibold text-xs sm:text-xl cursor-pointer">LOGIN</p></Link>
             )
           }
         </div>
